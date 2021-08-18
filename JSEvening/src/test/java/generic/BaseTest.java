cb5811a9f73658af54b92f70e59e72739b98b495
user <javagalbhanu2@gmail.com>
2021-08-18 20:57:51 +0530

    7PM Aug18

--- JSEvening/src/test/java/generic/BaseTest.java
+++ JSEvening/src/test/java/generic/BaseTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,12 +12,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest implements IConst {
 	public WebDriver driver;
-
+	public WebDriverWait wait;
+	
 	@BeforeMethod
 	public void openApp()
 	{
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
+		wait=new WebDriverWait(driver,ETO);
 		driver.manage().timeouts().implicitlyWait(ITO,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(APP_URL);
--- JSEvening/src/test/java/generic/IConst.java
+++ JSEvening/src/test/java/generic/IConst.java
@@ -2,18 +2,7 @@ package generic;
 
 public interface IConst {
 	int ITO=10;
-	String APP_URL="http://www.google.com";
+	String APP_URL="https://demo.actitime.com/login.do";
 	int ETO=10;
+	String XL_PATH="./data/input.xlsx";
 }
-/*1. create generic package under src/test/java
- *2. create IConst interface inside generic package
- *3. create BaseTest class inside generic package
- *4. create Excel class inside generic package
- *5. Create 'script' package under src/test/java
- *6. Create a TestNG class 'Test1' which extends BaseTest
- *7. run Test1
- * 
- * 
- * 
- */
-
