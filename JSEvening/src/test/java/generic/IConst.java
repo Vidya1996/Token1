bbe49b83f1cb0d0195e8348b32dcdc159a9ff4cc
user <javagalbhanu2@gmail.com>
2021-08-19 19:40:50 +0530

    Grid

--- JSEvening/src/test/java/generic/IConst.java
+++ JSEvening/src/test/java/generic/IConst.java
@@ -5,4 +5,7 @@ public interface IConst {
 	String APP_URL="https://demo.actitime.com/login.do";
 	int ETO=10;
 	String XL_PATH="./data/input.xlsx";
+	
+	String dURL="http://localhost:4444/wd/hub";
+	String dBrowser="chrome";
 }
